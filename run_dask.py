3d69f18d2ccca1adb9a91349bd132188354872f6
Kuan Butts <kuanbutts@gmail.com>
2017-04-14 15:22:13 -0700

    dask version of spatial indexed apply

--- run_dask.py
+++ run_dask.py
@@ -1,7 +1,9 @@
 import sys, csv, time
-import pandas as pd
+from dask import dataframe as dd
 import geopandas as gpd
+import pandas as pd
 from shapely.wkt import loads
+
 
 # log/monitor performance
 start_time = time.time()
@@ -10,62 +12,82 @@ def log(message):
     print('{:,.3f}s: {}'.format(time.time()-start_time, message))
     start_time = time.time()
 
-print('\n\n###################')
-print('Running without Dask first.')
-print('###################\n')
 
 # load in example geometry data
 filepath = './example_geometries.csv'
-df = pd.read_csv(filepath)
-log('loaded csv')
+init_df = pd.read_csv(filepath)
 
-# default crs projection for example geometries
-crs = {'init': 'epsg:32154'}
 
-# create a geometry geoseries from the geometry column's wkt string
-geometry = gpd.GeoSeries(df['geometry'].map(lambda x: loads(x)))
-log('created geometry')
+length_options = [500, 1000, 5000, 10000, 20000]
+for subset_length in length_options:
+    # take a subset of the dataframe per increment
+    print('\nRunning on subset length: ' + str(subset_length) + ' rows')
+    df = init_df.head(subset_length)
 
-# take a subset of the pandas df and convert to a gdf via geopandas
-gdf = gpd.GeoDataFrame(data=df[['id', 'value']], crs=crs, geometry=geometry)
+    # =====================
+    # First steps: prepare the GeoDataFrame
+    # =====================
 
-# ...now index the new gdf
-gdf = gdf.set_index('id')
-log('converted df to gdf')
+    # create a geometry geoseries from the geometry column's wkt string
+    crs = {'init': 'epsg:32154'}
+    geometries = gpd.GeoSeries(df['geometry'].map(lambda x: loads(x)))
 
-# create a spatial index to be used in a lambda/apply loop
-sidx = gdf.sindex
-log('created spatial index')
+    # take a subset of the pandas df and convert to a gdf via geopandas
+    gdf = gpd.GeoDataFrame(data=df[['id', 'value']], crs=crs, geometry=geometries)
 
-# we'll do an aggregation based off of an arbitrary buffer distance
-gdf['buffer'] = gdf['geometry'].buffer(500)
-log('buffered geometries')
+    # ...now index the new gdf (id is integer dtype)
+    gdf = gdf.set_index('id')
 
-# this is the summary/aggregation process we'll perform on each row
-def summarize(buffer):
-	# access sidx, gdf from global scope
-    possible_matches_index = list(sidx.intersection(buffer.bounds))
-    possible_matches = gdf.iloc[possible_matches_index]
-    precise_matches = possible_matches[possible_matches.intersects(buffer)]
-    return precise_matches['value'].sum()
+    # we'll do an aggregation based off of an arbitrary buffer distance
+    gdf['buffer'] = gdf['geometry'].buffer(5000)
 
-# execute the summarize operation on each row of the 
-# sums = gdf['buffer'].map(summarize)
-# log('calculated value sums')
+    print('\tFinished initial set up steps. Running Dask operation...')
 
-# print(sums.head())
+    # we can convert from a geodataframe to dask
+    dd_gdf = dd.from_pandas(gdf, npartitions=3)
 
-print('\n\n###################')
-print('Now utilizing Dask.')
-print('###################\n')
+    # =====================
+    # Next: define and apply the lambda function
+    # =====================
 
-# import relevant dask lib
-from dask import dataframe as dd
+    # update geometries global to preserve same index
+    geometries = gdf.geometry
+    sidx = gdf.sindex
 
-# can we conver from a geodataframe to dask?
-dd_gdf = dd.from_pandas(gdf, npartitions=3)
-log('converted gdf to dask dataframe')
+    # we want to update this column via summarize operation
+    dd_gdf['sum'] = 0.0
 
-print(dd_gdf.head())
+    # this is the summary/aggregation process we'll perform on each row
+    def summarize(row, sidx, dgdf, usecompute):
+        # we use dgdf from global scope
+        buffer = row['buffer']
 
-print(dd_gdf.head().bounds)
+        # utilizing precalc'd spatial indices
+        possible_matches_index = list(sidx.intersection(buffer.bounds))
+        possible_matches = dgdf.iloc[possible_matches_index]
+
+        where_intersects = possible_matches.intersects(buffer)
+        dgdf['intersects'] = where_intersects
+        precise_matches = dgdf[dgdf['intersects'] == True]
+        
+        summed = precise_matches['value'].sum()
+        if usecompute is True:
+            pass
+            # summed = summed.compute()    
+        row['sum'] = summed
+        
+        return row
+
+    meta = dd_gdf.loc[5758].compute()
+    usecompute = True
+    dd_sums_op = dd_gdf.apply(lambda row: summarize(row, sidx, gdf.copy(), usecompute), axis=1, meta=meta)
+    dd_sums = dd_sums_op.compute()
+
+
+    log('Finished Dask run.')
+    # print(dd_sums[['value', 'sum']].head())
+
+    print('\tRunning original operation...')
+    usecompute = False
+    gdf.apply(lambda row: summarize(row, sidx, gdf.copy(), usecompute), axis=1)
+    log('Finished normal method run.')
